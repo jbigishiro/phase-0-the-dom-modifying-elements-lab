4feb6fa66151f8bb16bc106e53982cff27909faa
Justin Bigishiro <justin.rvd@gmail.com>
2023-04-07 03:30:16 -0500

    adds myfile

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const main=document.getElementById('main');
+main.remove()
+const newHeader=document.createElement('h1')
+newHeader.setAttribute('id', 'victory')
+const name='Justin'
+newHeader.textContent=`${name} is the champion`
